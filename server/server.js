da9c27f5326007f7c4c6e8242033ed00e23aa70b
Rorisang-M <rorisang.work@gmail.com>
2023-09-12 11:35:37 +0200

    created test controllers and routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/todos', require('./routes/todo_route'))
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
